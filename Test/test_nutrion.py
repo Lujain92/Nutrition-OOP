ad12371d935da8c54cb6231892cd27d3b6e20bf5
Bashra99 <telfahb@gmail.com>
2022-10-15 15:06:33 +0000

    add 7 test

--- Test/test_nutrion.py
+++ Test/test_nutrion.py
@@ -61,6 +61,8 @@ def test_six():
     actual=item.get_fat()
     expected=9.8
     assert actual== expected
+
+  
 # @pytest.mark.skip
 # def test_seven():
 #     recepies=Recepies()
@@ -78,3 +80,41 @@ def test_six():
 #     expected=""
 #     assert actual== expected    
 # @pytest.mark.skip
+def test_nine():
+    item=Nutrition()
+    item.set_query('fries')
+    item.get_data()
+    actual=item.get_data()
+    expected=[
+       {
+    "name": "fries",
+    "calories": 317.7,
+    "serving_size_g": 100,
+    "fat_total_g": 14.8,
+    "fat_saturated_g": 2.3,
+    "protein_g": 3.4,
+    "sodium_mg": 212,
+    "potassium_mg": 124,
+    "cholesterol_mg": 0,
+    "carbohydrates_total_g": 41.1,
+    "fiber_g": 3.8,
+    "sugar_g": 0.3
+  }
+    ]
+    assert actual== expected
+def test_ten():
+    item=Nutrition()
+    item.set_query('fries')
+    item.get_data()
+    actual=item.get_calories()
+    expected=317.7
+    assert actual== expected
+  
+def test_eleven():
+    item=Nutrition()
+    item.set_query('fries')
+    item.get_data()
+    actual=item.get_protein()
+    expected=3.4
+    assert actual== expected
+    
